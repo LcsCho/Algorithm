9ef03e0cbeb4dde1c49987e4f9ea132418a3ceba
Lucas Cho <chojunhee98@gmail.com>
2023-06-28 12:19:00 +0900

    [Bronze III] Title: 곱셈, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int a, b;
+	cin >> a;
+	cin >> b;
+	
+	cout << a * (b % 10) << "\n";
+	cout << a * (b % 100 / 10) << "\n";
+	cout << a * (b / 100) << "\n";
+	cout << a * b;	
+	
+	return 0;
+}
