a17b9dda8c74907d701745f93c91a9ca0587cdb5
Lucas Cho <chojunhee98@gmail.com>
2023-06-28 15:22:53 +0900

    [Bronze III] Title: 공 넣기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/10810. 공 넣기/공 넣기.cc
+++ 백준/Bronze/10810. 공 넣기/공 넣기.cc
@@ -4,26 +4,23 @@ using namespace std;
 int main() {
 	int N, M;
 	cin >> N >> M;
-    
-    vector<int> baskets(N, 0);  // 초기에 모든 바구니에 0으로 초기화
-
-for (int i = 0; i < M; i++) {
-    int start, end, ball;
-    cin >> start >> end >> ball;
-
-    // 시작 바구니부터 끝 바구니까지 공의 번호로 채우기
-    for (int j = start - 1; j <= end - 1; j++) {
-        baskets[j] = ball;
-    }
-}
-
-// 결과 출력
-for (int i = 0; i < N; i++) {
-    cout << baskets[i] << " ";
-}
-
-return 0;
-
+	vector<int> baskets(N, 0);  // 초기에 모든 바구니에 0으로 초기화
+	
+	for (int i = 0; i < M; i++) {
+	    int start, end, ball;
+	    cin >> start >> end >> ball;
+	
+	    // 시작 바구니부터 끝 바구니까지 공의 번호로 채우기
+	    for (int j = start - 1; j <= end - 1; j++) {
+	        baskets[j] = ball;
+	    }
+	}
+	
+	// 결과 출력
+	for (int i = 0; i < N; i++) {
+	    cout << baskets[i] << " ";
+	}
 	
 	return 0;
+
 }
