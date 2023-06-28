1eed859bb9c7c07325ce7825ad27f7ebd469ee74
Lucas Cho <chojunhee98@gmail.com>
2023-06-28 13:39:12 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int longInt = 0;
+	cin >> longInt;
+	int count = longInt / 4;
+	
+	for (int i = 0; i < count; i++)
+		cout << "long ";
+		
+	cout << "int";
+	return 0;
+}
