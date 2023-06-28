5b38e05d40e463faa049a067cfc7caa8f646c6d2
Lucas Cho <chojunhee98@gmail.com>
2023-06-28 13:28:19 +0900

    [Bronze V] Title: A+B - 3, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int a, b, c;
+	cin >> a;
+	
+	for (int i = 0; i < a; i++) {
+		cin >> b >> c;
+		cout << b + c << "\n";
+	}
+	
+	return 0;
+}
