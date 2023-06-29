5f9b71aea88aa775f53ed76cf2282fec8b6102f4
Lucas Cho <chojunhee98@gmail.com>
2023-06-29 10:20:20 +0900

    [Bronze III] Title: 최댓값, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2566. 최댓값/최댓값.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int a, n, m, sum = 0;
+	for (int i = 1; i <= 9; i++) {
+		for (int j = 1; j <= 9; j++) {
+			cin >> a;
+			if (a >= sum) {
+				sum = a;
+				n = i;
+				m = j;
+			}
+		}
+	}
+	cout << sum << "\n";
+	cout << n << ' ' << m << "\n";
+	
+	return 0;
+}
