73d31adfc3a2bb1599cbc69798fc211d4f369f65
Lucas Cho <chojunhee98@gmail.com>
2023-07-02 17:05:26 +0900

    [Silver V] Title: 알고리즘 수업 - 점근적 표기 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/24313. 알고리즘 수업 － 점근적 표기 1/알고리즘 수업 － 점근적 표기 1.cc
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int a1, a0, c, n0; cin >> a1 >> a0 >> c >> n0;
+	cout << ((a1 * n0 + a0 <= c * n0) && (c - a1 >= 0));
+}
