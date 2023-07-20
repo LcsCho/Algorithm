02492c35f29f18e26190026a6e885bab0228c5b5
Lucas Cho <chojunhee98@gmail.com>
2023-07-20 20:44:54 +0900

    [Bronze I] Title: ROT13, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11655. ROT13/ROT13.cc
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h> 
+
+using namespace std; 
+
+string str;
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	getline(cin, str);
+	string rotStr[str.length()];
+	for (int i = 0; i < str.length(); i++) {
+		if ((str[i] >= 'a' && str[i] <= 'm') || (str[i] >= 'A' && str[i] <= 'M')) {
+			char cPlus = str[i] + 13;
+			rotStr[i] = cPlus;
+		}
+		else if ((str[i] >= 'n' && str[i] <= 'z') || (str[i] >= 'N' && str[i] <= 'Z')) {
+			char cMinus =  str[i] - 13;
+			rotStr[i] = cMinus;
+		}
+		else {
+			char c = str[i];
+			rotStr[i] = c;
+		}
+	}
+
+	for (int i = 0; i < str.length(); i++) cout << rotStr[i];
+
+	return 0;
+}
