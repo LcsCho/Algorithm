9bdee75cdb17d839043a44c699136069efd56479
Lucas Cho <chojunhee98@gmail.com>
2023-07-22 22:07:20 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.cc
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int t, n;
+string a, b;
+
+using namespace std;
+
+int main() {
+    cin >> t;
+
+    while (t--) {
+        map<string, int> m;
+        cin >> n;
+        for (int i = 0; i < n; i++) {
+            cin >> a >> b;
+            m[b]++;
+        }
+
+        long long ret = 1;
+        for (auto c : m)  ret *= ((long long)c.second + 1);
+        ret--;
+        cout << ret << "\n";
+    }
+
+    return 0;
+}
