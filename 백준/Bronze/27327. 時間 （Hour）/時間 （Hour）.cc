6bfd8b054e0cc2063d4b7bc2f09dbc1503c7e337
Lucas Cho <chojunhee98@gmail.com>
2023-07-27 16:39:07 +0900

    [Bronze V] Title: 時間 (Hour), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27327. 時間 （Hour）/時間 （Hour）.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int x;
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cin >> x;
+    cout << x * 24;
+    
+    return 0;
+}
