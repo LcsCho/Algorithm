5030fbefa7ebea0801361bf78bdbfc2e2369d020
Lucas Cho <chojunhee98@gmail.com>
2023-07-27 12:30:54 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string s;
+int main() {
+	cin >> s;
+	
+	if (s == "SONGDO") cout << "HIGHSCHOOL";
+	else if (s == "CODE") cout << "MASTER";
+	else if (s == "2023") cout << "0611";
+	else if (s == "ALGORITHM") cout << "CONTEST";
+	return 0;
+}
