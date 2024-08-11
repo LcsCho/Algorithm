e5a5592f202ba090b58a5c081eef25a9f127e859
Jun-hee Cho <chojunhee98@gmail.com>
2024-08-11 18:13:17 -0400

    [level 3] Title: 조건에 맞는 사용자 정보 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
+++ 프로그래머스/3/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
@@ -1,21 +1,11 @@
-SELECT 
-    UGU.USER_ID, 
-    UGU.NICKNAME, 
-    CONCAT(UGU.CITY, ' ', UGU.STREET_ADDRESS1, ' ', UGU.STREET_ADDRESS2) AS 전체주소,
-    CONCAT(SUBSTRING(UGU.TLNO, 1, 3), '-', SUBSTRING(UGU.TLNO, 4, 4), '-', SUBSTRING(UGU.TLNO, 8, 4)) AS 전화번호
-FROM 
-    USED_GOODS_USER UGU
-JOIN 
-    (SELECT 
-         WRITER_ID, 
-         COUNT(*) AS POST_COUNT 
-     FROM 
-         USED_GOODS_BOARD 
-     GROUP BY 
-         WRITER_ID 
-     HAVING 
-         COUNT(*) >= 3) UGB
-ON 
-    UGU.USER_ID = UGB.WRITER_ID
-ORDER BY 
-    UGU.USER_ID DESC;
+-- 코드를 입력하세요
+SELECT U.USER_ID, U.NICKNAME, 
+    concat(u.city, ' ', u.street_address1, ' ', u.street_address2) as 전체주소,
+    CONCAT(SUBSTRING(u.tlno, 1, 3), '-', SUBSTRING(U.TLNO, 4, 4), '-', SUBSTRING(U.TLNO, 8, 4))  AS 전화번호
+from
+    used_goods_board b
+    inner join used_goods_user u
+    on b.writer_id = u.user_id
+GROUP BY b.writer_id
+HAVING COUNT(*) >= 3
+ORDER BY u.user_id desc
